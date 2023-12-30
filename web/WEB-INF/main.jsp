b8d02ee3f547e76a27172a2c711491eecba7b2cf
ZY <1433861364@qq.com>
2023-12-30 12:26:04 +0800

    美化前端页面，添加粒子特效、轮播图

--- web/WEB-INF/main.jsp
+++ web/WEB-INF/main.jsp
@@ -20,19 +20,20 @@
 </head>
 <body>
 <div id="background">
-    <canvas id="canvas">111</canvas>
+    <canvas id="canvas" width="300px" height="140px">111</canvas>
 </div>
 
 <div class="content" >
     <div class="top">
         <div class="top_left">
+            <canvas id="slogan"></canvas>
             <ol class="news_switch">
                 <span>·</span>
-                <li><a>公告</a></li>
+                <li><a href="#">公告</a></li>
                 <span>·</span>
-                <li><a>活动</a></li>
+                <li><a href="#">活动</a></li>
                 <span>·</span>
-                <li><a>新闻</a></li>
+                <li><a href="#">新闻</a></li>
                 <span>·</span>
             </ol>
         </div>
@@ -55,21 +56,112 @@
 
     </div>
     <div class="body">
-        <div class="Chart">
-            轮播图
+        <div class="body_left">
+            <div class="wrap">
+                <ul class="list">
+                    <li class="item active"><img src="./img/1.jpg"></li>
+                    <li class="item"><img src="./img/2.jpg"></li>
+                    <li class="item"><img src="./img/3.jpg"></li>
+                    <li class="item"><img src="./img/4.jpg"></li>
+                    <li class="item"><img src="./img/5.jpg"></li>
+                </ul>
+                <ul class="pointList">
+                    <li class="point active" data-index=0></li>
+                    <li class="point" data-index=1></li>
+                    <li class="point" data-index=2></li>
+                    <li class="point" data-index=3></li>
+                    <li class="point" data-index=4></li>
+                </ul>
+                <div class="btn" id="leftBtn">
+                    <
+                </div>
+                <div class="btn" id="rightBtn">
+                    >
+                </div>
+            </div>
+            <div class="news">
+                新闻
+                <ul>
+                    <c:forEach items="${sessionScope.newsList}" var="news">
+                        <li class="newsList">
+                            <a href="#">
+                                <span class="newsDate">${news.publishTime}</span>
+                                <span class="newsFlag">新闻</span>
+                                <h1 class="newsContent">${news.content}</h1>
+                            </a>
+                        </li>
+                    </c:forEach>
+                </ul>
+            </div>
         </div>
-
-        <div class="news">
-            新闻
-            <ul>
-                <c:forEach items="${sessionScope.newsList}" var="news">
-                    <li>${news.content}</li>
-                </c:forEach>
-            </ul>
+        <div class="body_right">
+            <canvas id="Logo" width="500px" height="500px"></canvas>
         </div>
     </div>
 </div>
+<div class="footer-wrapper">
+    <ul class="footer-links">
+        <li class="footer-links-item"> <a href="#" class="footer-links-link">个人信息保护政策</a> </li>
+        <li class="footer-links-item"> <a href="#" class="footer-links-link">儿童个人信息保护政策</a> </li>
+        <li class="footer-links-item"> <a href="#" class="footer-links-link">使用许可及服务协议</a> </li>
+        <li class="footer-links-item"> <a href="#" class="footer-links-link">应用权限</a> </li>
+        <li class="footer-links-item"> <a href="#" class="footer-links-link">家长监护</a> </li>
+        <li class="footer-links-item"> <a href="#" class="footer-links-link">联系我们</a> </li>
+    </ul>
+    <div class="footer-slot"></div>
+    <ul class="footer-qualifications">
+        <li class="footer-qualifications-item"> <a href="#" rel="noopener noreferrer"> 沪ICP备17022476号-1 </a> </li>
+        <li class="footer-qualifications-item">沪网文〔2018〕0803-067号</li>
+        <li class="footer-qualifications-item">ISBN 978-7-498-05646-7</li>
+        <li class="footer-qualifications-item"> 国新出审〔2019〕49号 </li>
+        <li class="footer-qualifications-item policy">
+            <a class="footer-qualifications-item-policy"
+               href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010402005145"
+               target="_blank"
+               rel="noopener noreferrer">
+                <img class="footer-qualifications-item-icon"
+                     src="https://web.hycdn.cn/hg_web_sdk/lib/assets/policy.2c053d4c81fc2b352043.png">
+                沪公网安备 31010402005145号
+            </a>
+        </li>
+        <li class="footer-qualifications-item anti-fraud">
+            <div class="anti-fraud-container">
+                <img
+                    src="https://web.hycdn.cn/hg_web_sdk/lib/assets/anti-fraud.b04767f0bd0747af1db9.jpg"
+                    class="footer-qualifications-item-icon">
+                <span class="anti-fraud-text">
+                    亲爱的市民朋友，上海警方反诈劝阻电话“962110”系专门针对避免您财产被骗受损而设，请您一旦收到来电，立即接听。
+                </span>
+            </div>
+        </li>
+    </ul>
+    <div class="footer-claim">
+        <p class="footer-claim-info">
+            <span>开发者：<span class="footer-claim-info developer">上海鹰角网络科技有限公司</span></span>
+            <br>
+            <span>游戏当前版本：<span class="footer-claim-info version">2.1.41</span></span>&nbsp;
+            <span>游戏更新时间：<span class="footer-claim-info update">2023/12/05</span></span>
+        </p>
+        <p class="footer-claim-text"> <span>本网络游戏适合年满12周岁以上的用户使用；</span>
+            <span>为了您的健康，请合理控制游戏时间。</span>
+        </p>
+        <p class="footer-claim-text"> <span>抵制不良游戏 拒绝盗版游戏 注意自我保护 谨防受骗上当</span>
+            <span>适度游戏益脑 沉迷游戏伤身 合理安排时间 享受健康生活</span>
+        </p>
+    </div>
+    <div class="footer-copyright">
+        <span>Copyright ©2017 - 2023 上海鹰角网络科技有限公司</span>
+        <span>上海市嘉定区南翔镇银翔路799号昌辉大厦504-1室</span>
+        <span>电话：021-64399377</span>
+    </div>
+    <div class="footer-logo">
+        <img class="footer-company-logo"
+             src="https://web.hycdn.cn/hg_web_sdk/lib/assets/copyright.4c7484258fbab8b551aa.png">
+        <img class="footer-age-logo"
+             src="https://web.hycdn.cn/hg_web_sdk/lib/assets/age-tip.5e9b351af134c60314ab.png">
+    </div>
+</div>
 </body>
-<script type="text/javascript" src=".././js/main.js"></script>
+<script type="text/javascript" src="./js/main.js"></script>
 </html>
 
--- web/js/main.js
+++ web/js/main.js
@@ -12,7 +12,7 @@ window.onload = function () {
     // 定义一个粒子数组
     let particlesArray = [];
     // 定义页面内粒子的数量
-    let count = 60;
+    let count = 20;
 
     // 定义粒子类
     class Particle {
@@ -81,11 +81,211 @@ window.onload = function () {
 
     // 设置定时器
     setInterval(draw, 10);
-};
-//
 
-//以上内容主要借鉴自
-// 掘金剪影变粒子https://juejin.cn/post/7054488650410622984#heading-1
-// 跨域问题1 https://blog.csdn.net/Treeee_/article/details/111996118
-// 跨域问题2 https://blog.csdn.net/G294992891/article/details/109279415?utm_medium=distribute.pc_category.none-task-blog-hot-11.nonecase&depth_1-utm_source=distribute.pc_category.none-task-blog-hot-11.nonecase
-//但是修正看内容让其可以实际运行，跨域问题其实没有解决，还是时有时无
+    {
+        function draw2(id,url,gap,rgbmin,rgbmax,color,size,imgW,imgH) {
+            const canvas = document.getElementById(id); //读取到幕布
+            const ctx = canvas.getContext("2d"); //绘制2d样式
+            const img = new Image(); //新建一个图片对象
+            img.src = url + "?" + new Date().getTime();
+
+            img.setAttribute("crossOrigin", "Anonymous");
+
+            let imgData = null; //存图片的像素
+            let pointArr = []; //定义一个数组，用来存储点
+
+            img.onload = function () {
+                //图片加载时运行的
+                // imgH = imgW * (img.height / img.width); //等比计算高度
+                ctx.drawImage(img, 0, 0, imgW, imgH); //从画布00位置画出图像
+                imgData = ctx.getImageData(0, 0, imgW, imgH).data; //得到这个图像的所有位置的rgba信息
+                ctx.clearRect(0, 0, canvas.width, canvas.height); //这里是把幕布先清空
+                // console.log(imgData); //看看有没有获取到信息用的
+                pointInit(imgData); //用来确定哪些位置有点
+                move();
+            };
+
+            function pointInit(imgData) {
+                for (var h = 0; h < imgH; h += gap) {
+                    for (var w = 0; w < imgW; w += gap) {
+                        //遍历每个位置
+                        var position = (imgW * h + w) * 4; //因为data是把矩阵排列一个长数组，每个位置有rgba4个元素，所以这样可以算出这个点在数组里的位置
+                        var r = imgData[position],
+                            g = imgData[position + 1],
+                            b = imgData[position + 2]; //读取每个点的rgb
+
+                        if (r + g + b >= rgbmin && r + g + b <= rgbmax) {
+                            //这里是判断什么时候加点，要根据图片颜色改（从0到765越来越白）
+                            pointArr.push(new Point(size, w, h)); //在数组里添加一个点(1是点的大小可以改)
+                        }
+                    }
+                }
+            }
+            class Point {
+                //定义点对象
+                constructor(size, w, h) {
+                    this.orw = w;
+                    this.orh = h; // 保留图像初始位置，方便粒子聚合成图像
+
+                    this.x = Math.random() * canvas.width;
+                    this.y = Math.random() * canvas.height; // 随机位置
+                    this.size = size; //点的大小
+                    this.w = w;
+                    this.h = h;
+                    // 速度
+                    this.spx = (w - this.x) / 2 / 20;
+                    this.spy = (h - this.y) / 2 / 20;
+                }
+
+                update() {
+                    // 圆点每次位置变化
+                    this.spx = (this.w - this.x) / 40;
+                    this.spy = (this.h - this.y) / 40; //这也是来当速度的，可以看作步长
+                    if (Math.abs(this.w - this.x) <= Math.abs(this.spx)) {
+                        this.x = this.w; //如果随机位置离实际位置小于步长，就相当于到位了
+                    } else {
+                        this.x += this.spx; //如果没到就加个步长
+                    }
+                    if (Math.abs(this.h - this.y) <= Math.abs(this.spy)) {
+                        this.y = this.h;
+                    } else {
+                        this.y += this.spy;
+                    }
+                }
+                // 渲染圆点
+                rander() {
+                    ctx.beginPath(); //开始画图
+                    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2); //定义成圆点(其实画的是空心圆)
+                    ctx.fillStyle = color; //点的颜色
+                    ctx.fill(); //填充
+                    ctx.closePath(); //停止绘图
+                }
+            }
+            function move() {
+                //点的移动
+                ctx.clearRect(0, 0, canvas.width, canvas.height); //让点显现之后再消失，看着就像移动啦，删掉会留下轨迹，还挺有意思
+                pointArr.forEach((point) => {
+                    //遍历每个点
+                    point.update(); //更新点的内容
+                    point.rander(); //画图
+                });
+
+                // 代替setTimeout提升性能
+                requestAnimationFrame(move); //我的理解是这就是循环吧，可以一直回代画出来每个点？
+            }
+
+        };
+        let id = "slogan";
+        let url = `./img/slogan.png`;
+        let gap = 2;
+        let rgbmin = 100;
+        let rgbmax = 500;
+        let color = "#555";
+        let size = 2;
+        let imgW = 300; //固定宽度
+        let imgH = 140;
+        draw2(id,url,gap,rgbmin,rgbmax,color,size,imgW,imgH)
+
+        id = "Logo";
+        url = `./img/img.png`;
+        gap = 7;
+        rgbmin = 100;
+        rgbmax = 200;
+        color = "#eee";
+        size = 2;
+        imgW = 550; //固定宽度
+        imgH = 500;
+        draw2(id,url,gap,rgbmin,rgbmax,color,size,imgW,imgH)
+    }
+
+
+    {
+        let items = document.querySelectorAll(".item");//图片节点
+        let points = document.querySelectorAll(".point")//点
+        let left = document.getElementById("leftBtn");
+        let right = document.getElementById("rightBtn");
+        let all = document.querySelector(".wrap")
+        let index = 0;
+        let time = 0;//定时器跳转参数初始化
+
+
+        //封装一个清除active方法
+        let clearActive = function () {
+            for (i = 0; i < items.length; i++) {
+                items[i].className = 'item';
+            }
+            for (j = 0; j < points.length; j++) {
+                points[j].className = 'point';
+            }
+        }
+
+        //改变active方法
+        let goIndex = function () {
+            clearActive();
+            items[index].className = 'item active';
+            points[index].className = 'point active'
+        }
+        //左按钮事件
+        let goLeft = function () {
+            if (index === 0) {
+                index = 4;
+            } else {
+                index--;
+            }
+            goIndex();
+        }
+
+        //右按钮事件
+        let goRight = function () {
+            if (index < 4) {
+                index++;
+            } else {
+                index = 0;
+            }
+            goIndex();
+        }
+
+
+        //绑定点击事件监听
+        left.addEventListener('click', function () {
+            goLeft();
+            time = 0;//计时器跳转清零
+        })
+
+        right.addEventListener('click', function () {
+            goRight();
+            time = 0;//计时器跳转清零
+        })
+
+        for (i = 0; i < points.length; i++) {
+            points[i].addEventListener('click', function () {
+                let pointIndex = this.getAttribute('data-index')
+                index = pointIndex;
+                goIndex();
+                time = 0;//计时器跳转清零
+            })
+        }
+        //计时器轮播效果
+        let timer;
+
+        function play() {
+            timer = setInterval(() => {
+                time++;
+                if (time === 20) {
+                    goRight();
+                    time = 0;
+                }
+            }, 100)
+        }
+
+        play();
+        //移入清除计时器
+        all.onmousemove = function () {
+            clearInterval(timer)
+        }
+        //移出启动计时器
+        all.onmouseleave = function () {
+            play();
+        }
+    }
+}
