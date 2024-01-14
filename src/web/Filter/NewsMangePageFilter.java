b5425f702e84cb61894a5dea837c292de9af68ec
ZY <1433861364@qq.com>
2024-01-14 21:24:15 +0800

    使用Filter实现管理页面的过滤

---
+++ src/web/Filter/NewsMangePageFilter.java
@@ -0,0 +1,32 @@
+package web.Filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class NewsMangePageFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession();
+        Object loginAdmin = session.getAttribute("loginAdmin");
+        if (loginAdmin != null) {
+            filterChain.doFilter(request,response);
+        } else {
+            response.sendRedirect("main");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
